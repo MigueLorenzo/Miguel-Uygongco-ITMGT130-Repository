001b96d82a1956fc3b5e84cb8e0b1c79232ba112
Miguel Lorenzo <miguel.uygongco@obf.ateneo.edu>
2022-04-14 14:45:53 +0800

    assignment 0 submission and COA

---
+++ assignment-0.py
@@ -0,0 +1,33 @@
+'''Assignment 0
+
+This assignment is a giveaway assignment.
+By completing this assignment, you will familiarize yourself with this
+    course's submission mechanics.
+'''
+
+def three_number_average(x, y, z):
+    '''Item 1.
+    Three Number Average. 3 points.
+
+    Returns the average of three numbers.
+
+    Parameters
+    ----------
+    x: int
+        the first number
+    y: int
+        the second number
+    z: int
+        the third number
+
+    Returns
+    -------
+    float
+        the average of x, y, and z
+    '''
+    # Write your code below this line
+    x = 5
+    y = 10
+    z = 15
+    avg = (x + y + z)/3
+    print('The average of the three numbers is = %0.2f' %avg)
